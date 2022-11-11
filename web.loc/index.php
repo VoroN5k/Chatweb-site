8450a8eb8537d135f7ac16bb79f1a0785844d900
VoroN5k <Volovykartem@gmail.com>
2022-11-12 01:52:48 +0200

    All required changes
    separated header from other files, connected wth "include", two php files, new folder @exploit-exe

---
+++ web.loc/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chatweb </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php include'header.php'; ?>
+
+	<div class="main-content">
+		<div class="left-container">
+			<div class="container-left-list">
+				<a href="#" style="background-color: #04AA6D; color: white; margin-top: 5px;">Home</a>
+				<a href="#">Main</a>	
+				<a href="#">Help</a>
+				<a href="#">About</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer">
+		
+	</div>
+</body>
+</html>
