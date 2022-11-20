b0fcc29d07fdff0e1103a47135856d29d2c438bd
exploit-exe <oleshkathebest228@gmail.com>
2022-11-20 21:11:13 +0200

    modified:   web.loc/index.php 	modified:   web.loc/main.css 	new file:   web.loc/send-icon.png

--- web.loc/index.php
+++ web.loc/index.php
@@ -20,7 +20,31 @@
 		</div>
 
 		<div class="chat-container">
-			test testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest test
+
+			<div class="chat-messages">
+
+				<div class="message">
+					<p>Hello my dear dick</p>
+				</div>
+				<div class="message">
+					<p>Hello my dear dick</p>
+				</div>
+				<div class="message">
+					<p>Hello my dear dick</p>
+				</div>
+				<div class="message">
+					<p>Hello my dear dick</p>
+				</div>
+
+			</div>
+
+			<div class="input-bar">
+
+				<textarea class="input-message"></textarea>
+				<input type="image" src="send-icon.png" class="send-message" onclick="SendMessage()">
+
+			</div>
+
 		</div>
 
 	</div>
@@ -28,5 +52,22 @@
 	<div class="footer">
 		
 	</div>
+
+	<script>
+		function SendMessage() {
+			let message = document.getElementsByClassName("input-message")[0].value;
+			console.log(message);
+			let chat_messages = document.getElementsByClassName("chat-messages");
+			let div = document.createElement("div");
+			div.setAttribute("class", "message");
+			let p = document.createElement("p");
+			p.innerHTML = message;
+			div.appendChild(p);
+			chat_messages[0].appendChild(div);
+		}
+
+		
+
+	</script>
 </body>
 </html>
